bd174d18d0a25937c74b0f292cda5b4e3a6e59cb
Shashi Tiwari <tiwari.shashi@orangemantra.in>
2023-03-03 16:29:19 +0530

    • Added all files

---
+++ SampleCocoa3March/SampleCocoa3March.h
@@ -0,0 +1,18 @@
+//
+//  SampleCocoa3March.h
+//  SampleCocoa3March
+//
+//  Created by ShashiTiwari on 03/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleCocoa3March.
+FOUNDATION_EXPORT double SampleCocoa3MarchVersionNumber;
+
+//! Project version string for SampleCocoa3March.
+FOUNDATION_EXPORT const unsigned char SampleCocoa3MarchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleCocoa3March/PublicHeader.h>
+
+
